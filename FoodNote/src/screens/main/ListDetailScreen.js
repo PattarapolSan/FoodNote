72d0f8def41b86694a1670652b258285345771c4
PattarapolSangprakai <6322770403@g.siit.tu.ac.th>
2023-12-08 21:09:21 +0700

    set Navigation for List detail and Add list, and create logout function

--- FoodNote/src/screens/main/ListDetailScreen.js
+++ FoodNote/src/screens/main/ListDetailScreen.js
@@ -2,24 +2,20 @@ import React from "react";
 import { Text,View,Button } from "react-native";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const HomeScreen = ({navigation}) => {
+const ListDetailScreen = ({navigation}) => {
     AsyncStorage.getItem('user').then((value)=> {
         console.log(value);
     })
     return(
         <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-            <Text>Home Screen</Text>
+            <Text>List Detail Screen</Text>
             <Button 
-                title="go add list"
-                onPress={()=>{navigation.navigate("AddList")}}
-            />
-            <Button 
-                title="go list detail"
-                onPress={()=>{navigation.navigate("ListDetail")}}
+                title="go to home"
+                onPress={()=>{navigation.goBack()}}
             />
         </View>
 
     );
 }
 
-export default HomeScreen; 
+export default ListDetailScreen; 
--- FoodNote/src/screens/main/ProfileScreen.js
+++ FoodNote/src/screens/main/ProfileScreen.js
@@ -1,9 +1,27 @@
-import React from "react";
-import { Text } from "react-native";
+import React,{useState} from "react";
+import { Text,View,Button } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const ProfileScreen = ({navigation}) => {
+    const [user, setUser] = useState('No user');
+    AsyncStorage.getItem('user').then((value)=> {
+        console.log(value);
+        setUser(value);
+    })
+
+    const handleLogout = () => {
+        console.log("Logout");
+        AsyncStorage.removeItem('user');
+        navigation.replace("Landing");
+    }
     return(
-        <Text>Profile Screen</Text>
+        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+        <Text>{user}</Text>
+        <Button 
+            title="Log out"
+            onPress={handleLogout}
+        />
+    </View>
     );
 }
 
